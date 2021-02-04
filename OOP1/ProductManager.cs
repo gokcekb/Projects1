db10c2d75c86f31c0cd0201d64ea1814ea55ec61
Gökçe <kucukbayramgokce@gmail.com>
2021-02-04 12:15:47 +0300

    20 Ocak dersi

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //encapsulation
+        public void Add(Product product) //101
+        {
+            Console.WriteLine(product.ProductName + "eklendi.");
+            //product.ProductName = "Kamera";
+        }
+        public void Update (Product product)
+        {
+            Console.WriteLine(product.ProductName + "güncellendi.");
+        }
+
+        /* public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public void Topla2(int sayi1,int sayi2)
+        {
+            Console.WriteLine(sayi1 + sayi2);
+        }
+        */
+
+
+
+        /* 
+        public void BiseyYap(int sayi)
+        {
+            sayi = 99;
+        }
+        */
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product {Id = 2,CategoryId = 5,
+                UnitsInStock = 5,ProductName="Kalem",UnitPrice = 35};
+            
+            //PascalCase //camelCase
+            //case sensitive
+            ProductManager productManager = new ProductManager();
+            //heap                          //stack
+            productManager.Add(product1); //101 adres
+            Console.WriteLine(product1.ProductName);
+
+            /*
+            productManager.Topla2(3, 6);
+            int toplamaSonucu = productManager.Topla(3, 6);
+            Console.WriteLine(toplamaSonucu*2);
+            */
+
+
+
+            /* 
+            int sayi = 100;
+            productManager.BiseyYap(sayi);
+            Console.WriteLine(sayi);
+            */
+            
+
+            //int-double-bool... değer tip 
+            //diziler-class-abstract class-interface... referans tip
+
+
+        }
+    }
+}
